11c142b5f0a0744751c04a4fcae291b262749923
Luis Arriaga <larriaga@proficio.com>
2018-06-01 15:30:18 -0700

    Deleting master branch contents to substitute with Wazuh's stable branch.

--- framework/wazuh/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
-
-from __init__ import main
-
-main()
